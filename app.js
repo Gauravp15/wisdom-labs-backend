573d83e8b8d3bcf7010dbf92ff48119fc5852611
Gauravp15 <gauravprabhu15@gmail.com>
2025-05-06 22:09:05 +0530

    Updated code

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 require('dotenv').config();
 const cors = require("cors");
 const axios = require("axios");
-const bodyParser = require("body-parser");
 const helmet = require("helmet");
 
 const app = express();
@@ -12,15 +11,30 @@ const ENDPOINT = "https://script.google.com/macros/s/AKfycbxIh3jaUpbVKkuwejHPIYc
 
 app.use(cors());
 app.use(helmet());
-app.use(bodyParser.json());
+
+// Parse application/json
+app.use(express.json());
+
 
 app.post("/", (req, res) => {
-    let request = req.body;
+    let request = req;
+    console.log("Req body", request.body);
+    const params = new URLSearchParams();
+    for (const key in request.body) {
+      params.append(key, request.body[key]);
+    }
     axios
-    .post(`${ENDPOINT}?role=${request.role === "other" ? request["role-Comment"] : request.role}`, request)
+    .post(`${ENDPOINT}`,
+    params,
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
     .then(async resp => {
         if(resp.status === 200){
             let data = resp.data;
+            console.log("Data", data);
             res.status(200).send("Data captured");
         }
     }).catch(e => {
